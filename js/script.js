cb9b7d50b75530cfbf0772c8808f4e812429626b
RyanShaw21 <>
2021-05-03 16:04:52 +0000

    Created index.html, style.css, and script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Ryan-Shaw-2 All rights reserved
+//
+// Created by: Ryan-Shaw-2
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays, "Hello, World!"
+
+  document.getElementById('hello-world').innerHTML = 'Hello, World!'
+}
